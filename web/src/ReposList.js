2563258eca10e3b63a51cc586c794971caf19d56
Shane Phillips <shane@loongphillips.com>
2022-05-11 17:27:06 -0400

    Attempted to filter by language, did not succeed...

--- web/src/ReposList.js
+++ web/src/ReposList.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 
 const ReposList = () => {
   const [info, setInfo] = useState(null);
+  const [lang, setLang] = useState('');
   const handleSort = (a, b) => b.created_at.localeCompare(a.created_at);
   useEffect(() => {
     let sorted = null;
@@ -24,28 +25,60 @@ const ReposList = () => {
   return (
     <>
       <div>
-        {info &&
-          info.map((el) => {
-            return (
-              <>
-                <div key={Math.floor(Math.random() * 123456789)}>
-                  {el.full_name}
-                </div>
-                <div key={Math.floor(Math.random() * 123456789)}>
-                  {el.language}
-                </div>
-                <div key={Math.floor(Math.random() * 123456789)}>
-                  {el.description}
-                </div>
-                <div key={Math.floor(Math.random() * 123456789)}>
-                  {el.forks_count}
-                </div>
-                <div key={Math.floor(Math.random() * 123456789)}>
+        <p>(Don't press these, they don't work.)</p>
+        <button onClick={() => setLang('English')}>English</button>
+        <button onClick={() => setLang('French')}>French</button>
+        <button onClick={() => setLang('Typescript')}>Typescript</button>
+      </div>
+      <div>
+        {lang === ''
+          ? info &&
+            info.map((el) => {
+              return (
+                <>
+                  <div key={Math.floor(Math.random() * 123456789)}>
+                    {el.full_name}
+                  </div>
+                  <div key={Math.floor(Math.random() * 123456789)}>
+                    {el.language}
+                  </div>
+                  <div key={Math.floor(Math.random() * 123456789)}>
+                    {el.description}
+                  </div>
+                  <div key={Math.floor(Math.random() * 123456789)}>
+                    {el.forks_count}
+                  </div>
+                  {/* <div key={Math.floor(Math.random() * 123456789)}>
                   {el.created_at}
-                </div>
-              </>
-            );
-          })}
+                </div> */}
+                </>
+              );
+            })
+          : lang === 'English'
+          ? info &&
+            //This filter function does not work.
+            info.filter((el) => {
+              console.log(el.language === 'English');
+              return (
+                el.language === 'English' && (
+                  <>
+                    <div key={Math.floor(Math.random() * 123456789)}>
+                      {el.full_name}
+                    </div>
+                    <div key={Math.floor(Math.random() * 123456789)}>
+                      {el.language}
+                    </div>
+                    <div key={Math.floor(Math.random() * 123456789)}>
+                      {el.description}
+                    </div>
+                    <div key={Math.floor(Math.random() * 123456789)}>
+                      {el.forks_count}
+                    </div>
+                  </>
+                )
+              );
+            })
+          : null}
       </div>
       <div>x</div>
     </>
