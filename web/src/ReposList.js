d35fc7ffbfbd297e7d75960b528996bc77b11911
Shane Phillips <shane@loongphillips.com>
2022-05-11 16:48:59 -0400

    ISO dates in el.created_by sorted by reverse chronological order

--- web/src/ReposList.js
+++ web/src/ReposList.js
@@ -2,7 +2,10 @@ import { useEffect, useState } from 'react';
 
 const ReposList = () => {
   const [info, setInfo] = useState(null);
+  const handleSort = (a, b) => b.created_at.localeCompare(a.created_at);
   useEffect(() => {
+    let sorted = null;
+    let holder = null;
     fetch('http://localhost:4000/repos', {
       headers: {
         'Content-Type': 'application/json',
@@ -12,7 +15,10 @@ const ReposList = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log('data', data.data2);
-        setInfo(data.data2);
+        holder = data.data2;
+        sorted = holder.sort(handleSort);
+        console.log(sorted);
+        setInfo(sorted);
       });
   }, []);
   return (
@@ -20,7 +26,25 @@ const ReposList = () => {
       <div>
         {info &&
           info.map((el) => {
-            return <div>{el.id}</div>;
+            return (
+              <>
+                <div key={Math.floor(Math.random() * 123456789)}>
+                  {el.full_name}
+                </div>
+                <div key={Math.floor(Math.random() * 123456789)}>
+                  {el.language}
+                </div>
+                <div key={Math.floor(Math.random() * 123456789)}>
+                  {el.description}
+                </div>
+                <div key={Math.floor(Math.random() * 123456789)}>
+                  {el.forks_count}
+                </div>
+                <div key={Math.floor(Math.random() * 123456789)}>
+                  {el.created_at}
+                </div>
+              </>
+            );
           })}
       </div>
       <div>x</div>
